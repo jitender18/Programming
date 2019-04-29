5442586032b773a0d6637517082653cf7f7b35f2
Jitender <jitendephgt@gmail.com>
2019-04-29 18:21:14 -0500

    solutions

---
+++ Python/Programming_Solutions/capitalize.py
@@ -0,0 +1,49 @@
+# --- Directions
+# Write a function that accepts a string.  The function should capitalize the
+# first letter of each word in the string then return the capitalized string.
+# --- Examples
+#   capitalize('a short sentence') --> 'A Short Sentence'
+#   capitalize('a lazy fox') --> 'A Lazy Fox'
+#   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+
+# def capitalize(string):
+#     """
+#     Using the Python str.title() method
+#     """
+#     return string.title()
+
+# def capitalize(string):
+#     """
+#     Using a loop on the string
+#     """
+#     result = string[0].upper()
+#     for index in range(1, len(string)):
+#         if string[index-1] == " ":
+#             result += string[index].upper()
+#         else:
+#             result += string[index]
+#     return result
+
+def capitalize(string):
+    """
+    Using a loop on a list split from the string
+    """
+    words = []
+    for word in string.split(" "):
+    	for i in word:
+    		if i.isalpha():
+    			print("here")
+    			word = word.replace(i, i.upper())
+    			break
+        words.append(word)
+    return " ".join(words)
+
+
+print(capitalize('look, !it is working!'))
+
+
+
+
+
+
---
+++ Python/Programming_Solutions/chunk.py
@@ -0,0 +1,66 @@
+# --- Directions
+# Given a list and chunk size, divide the list into multiple nested lists within
+# a list where each nested list is of length size
+# --- Examples
+# chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+# chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+# chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+# chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+# chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+# def chunk(l, size):
+#     """
+#     Using while until the list is empty Emptying out the list using del 
+#     """
+#     chunked = []
+#     while len(l) > 0:
+#         chunked.append(l[0:size])
+#         del l[0:size]
+#     return chunked
+
+# def chunk(l, size):
+#     """
+#     Using while with an index counter
+#     """
+#     chunked = []
+#     index = 0
+#     while index < len(l):
+#         chunked.append(l[index:index+size])
+#         index += size
+#     return chunked
+
+# def chunk(l, size):
+#     """
+#     Using interation DOES NOT WORK!
+#     """
+#     chunked = []
+#     for element in l:
+#         last = chunked[len(chunked) - 1]
+#         if not last or len(last) == size:
+#             chunked.append([element])
+#         else:
+#             last.append(element)
+#     return chunked
+
+############################### Mine ##################################
+def chunk(l, size):
+	length = len(l)
+
+	chunk = []
+
+	for i in range(0, length+1, size):
+		chunk.append(l[i:i+size])
+	return chunk
+
+
+
+print(chunk([1, 2, 3, 4, 5], 2))
+
+
+
+
+
+
+
+
